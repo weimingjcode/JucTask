bef8deccfb1cbc2e5a690d8e3a078f20c559d2ad
weimingjcode <waczjm@126.com>
2020-01-04 00:30:10 +0800

    first commit

---
+++ src/main/java/com/crud/Controller/testController.java
@@ -0,0 +1,14 @@
+package com.crud.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class testController {
+    @RequestMapping("test")
+    @ResponseBody
+    public String test(){
+        return "test!!!!";
+    }
+}
